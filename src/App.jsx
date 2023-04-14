f7ea4183138af3de333d067e088e1e6ddbc38fbe
KRAUTH Mickael <mickaell.kr@gmail.com>
2023-04-14 20:44:03 +0200

    added linechart and fixed barchart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Cell, Tooltip, Legend } from "recharts";
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Cell, Tooltip, Legend } from "recharts";
 
 
 async function fetchData(id, el = false) {
@@ -18,7 +18,15 @@ async function fetchData(id, el = false) {
 function App() {
   const [data, setData] = useState([]);
   const [user, setUser] = useState([]);
-
+  const dataD = [
+    { day: 'M', n: 10 },
+    { day: 'T', n: 20 },
+    { day: 'W', n: 30 },
+    { day: 'Th', n: 25 },
+    { day: 'F', n: 15 },
+    { day: 'Sa', n: 5 },
+    { day: 'Su', n: 0 },
+  ];
   useEffect(() => {
     loadData();
   }, []);
@@ -46,86 +54,110 @@ function App() {
       </ul>
     );
   };
-
-
-
-
   return (
     <section className='mainPage'>
       <h1 className="bonjour">Bonjour <span className="firstName">{user.firstName}</span></h1>
-      <p className="bravo">Félicitation ! Vous avez explosé vos objectifs hier</p>
-      <BarChart width={835} height={320} data={data} barGap={8} >
-        <CartesianGrid
-          vertical={false}
-          horizontal={true}
-          strokeDasharray="3 3" // This creates the dotted lines
-          stroke="#DEDEDE"
-        />
-        <YAxis
-          yAxisId="left"
-          dataKey='kilogram'
-          axisLine={{
-            stroke: '#DEDEDE',
-            strokeWidth: 1,
-          }}
-          domain={["dataMin - 4", "dataMax + 5"]}
-          tickLine={false}
-          orientation="right" />
-        <YAxis
-          yAxisId="right"
-          dataKey="calories"
-          domain={[0, "dataMax + 20"]}
-          tick={false} // Add this line to hide tick marks
-          axisLine={false} // Add this line to hide the axis line
-        />
-        <Tooltip
-          content={({ payload }) => {
-            if (payload && payload.length) {
-              return (
-                <div className='tooltip'>
-                  <p className='weight'>{payload[0].payload.kilogram} kg</p>
-                  <p className='calories'>{payload[0].payload.calories} Kcal</p>
-                </div>
-              );
-            }
-            return null;
-          }}
-        />
-        <Legend
-          align="right"
-          verticalAlign="top"
-          content={<CustomLegend />}
-        />
+      <p className="bravo">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      <div className="chartsContainer">
+        <BarChart width={800} height={320} data={data} barGap={8} className='barChart'>
+          <CartesianGrid
+            vertical={false}
+            horizontal={true}
+            strokeDasharray="3 3" // This creates the dotted lines
+            stroke="#DEDEDE"
+          />
+          <YAxis
+            yAxisId="left"
+            dataKey='kilogram'
+            axisLine={{
+              stroke: '#DEDEDE',
+              strokeWidth: 1,
+            }}
+            domain={["dataMin - 4", "dataMax + 5"]}
+            tickLine={false}
+            orientation="right"
+            width={40} />
+          <YAxis
+            yAxisId="right"
+            dataKey="calories"
+            domain={[0, "dataMax + 20"]}
+            tick={false} // Add this line to hide tick marks
+            axisLine={false} // Add this line to hide the axis line
+            width={40}
+          />
+          <Tooltip wrapperStyle={{ outline: "none" }}
+            content={({ payload }) => {
+              if (payload && payload.length) {
+                return (
+                  <div className='tooltip'>
+                    <p className='weight'>{payload[0].payload.kilogram} kg</p>
+                    <p className='calories'>{payload[0].payload.calories} Kcal</p>
+                  </div>
+                );
+              }
+              return null;
+            }}
+          />
+          <Legend
+            align="right"
+            verticalAlign="top"
+            style={{ right: "34px" }}
+            content={<CustomLegend />}
+          />
 
-        <Bar yAxisId="left" dataKey='kilogram' fill='#282D30' name='Kilogram' barSize={7} legendType='circle' radius={[5, 5, 0, 0]}>
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill='#282D30' />
-          ))}
-        </Bar>
-        <Bar yAxisId="right" dataKey='calories' fill='#E60000' name='Calories' barSize={7} legendType='circle' radius={[5, 5, 0, 0]}>
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill='#E60000' />
-          ))}
-        </Bar>
-        <XAxis
-          dataKey='day'
-          axisLine={{
-            stroke: '#DEDEDE',
-            strokeWidth: 1,
-          }}
-          tickLine={false}
-          height={40}
-          tick={{
-            fill: '#9B9EAC',
-            fontSize: '14px',
-            dy: 10, // add some extra margin between tick and label
-          }}
-          tickMargin={15} // add margin between axis line and tick
-          tickFormatter={(value, index) => index + 1}
-        />
+          <Bar yAxisId="left" dataKey='kilogram' fill='#282D30' name='Kilogram' barSize={7} legendType='circle' radius={[5, 5, 0, 0]}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill='#282D30' />
+            ))}
+          </Bar>
+          <Bar yAxisId="right" dataKey='calories' fill='#E60000' name='Calories' barSize={7} legendType='circle' radius={[5, 5, 0, 0]}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill='#E60000' />
+            ))}
+          </Bar>
+          <XAxis
+            dataKey='day'
+            axisLine={{
+              stroke: '#DEDEDE',
+              strokeWidth: 1,
+            }}
+            tickLine={false}
+            height={40}
+            tick={{
+              fill: '#9B9EAC',
+              fontSize: '14px',
+              dy: 10, // add some extra margin between tick and label
+            }}
+            tickMargin={15} // add margin between axis line and tick
+            tickFormatter={(value, index) => index + 1}
+          />
 
-      </BarChart>
-
+        </BarChart>
+        <div className="lineContainer">
+          <h3 className="lineTitle">Durée moyenne des sessions</h3>
+          <LineChart width={270} height={220} data={dataD} stroke="#FFFFFF" fill="#FF0000" className='linechart'>
+            <CartesianGrid stroke='transparent' />
+            <XAxis dataKey="day"
+              axisLine={false} height={40} tickLine={false} tick={{ fill: "#fff", opacity: '0.5' }} />
+            <YAxis dataKey="n" tick={false}
+              axisLine={false} width={17} />
+            <Tooltip wrapperStyle={{ outline: "none" }}
+              content={({ payload }) => {
+                if (payload && payload.length) {
+                  return (
+                    <div className='tooltipLine'>
+                      <p >{payload[0].value} min</p>
+                    </div>
+                  );
+                }
+                return null;
+              }}
+            />
+            <Line type="monotone" dataKey="n" stroke="#fff"
+              strokeWidth={(index) => (index === 0 ? 1 : index * 100)} />
+          </LineChart>
+        </div>
+      </div>
     </section>
   )
 }
